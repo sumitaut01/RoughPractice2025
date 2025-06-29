e6e572a6697b0f4e6f30b900086051e6ed92edb7
sumit <sumitraut01@gmail.com>
2025-06-29 22:48:39 +0530

    new learning added

---
+++ Basics/basics/datastructure/R1_ArrayasStack.java
@@ -0,0 +1,13 @@
+package basics.datastructure;
+
+//A stack is a Last In, First Out (LIFO) data structure.
+//
+//        Think of:
+//        A pile of books → you add and remove only from the top
+//
+//        Push = add to top, Pop = remove from top
+
+
+
+public class R1_ArrayasStack {
+}
