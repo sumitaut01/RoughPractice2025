8ad8c81bd97989c12988b5a2c35405dc550308db
sumit <sumitraut01@gmail.com>
2025-09-14 16:21:18 +0530

    bdd info added

---
+++ src/main/java/basics/R_XScanner.java
@@ -0,0 +1,25 @@
+package basics;
+
+import java.util.Scanner;
+
+public class R_XScanner {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter name= >");
+        String name=sc.nextLine();
+        System.out.println("name is "+name);
+
+        System.out.println("Enter Age");
+        int Age=sc.nextInt();
+        System.out.println("Age is "+Age);
+
+        System.out.println("Enter Salary");
+        double sal=sc.nextDouble();
+        System.out.println("Salary is "+sal);
+
+
+    }
+}
---
+++ src/main/java/basics/R_XScopeWideningWhenOverriding.java
@@ -0,0 +1,27 @@
+package basics;
+
+public class R_XScopeWideningWhenOverriding {
+
+
+
+}
+
+
+
+class Human{
+
+ public void method1(){
+     System.out.println("Some impl");
+ }
+}
+
+
+class Male extends Human{
+
+//    @Override
+//    protected void method1(){
+//        //'method1()' in 'basics.Male' clashes with 'method1()' in 'basics.Human';
+//        // attempting to assign weaker access privileges ('protected'); was 'public'
+//        System.out.println("Some impl");
+//    }
+}
